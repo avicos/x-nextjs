10ebbc5a8041b4404c8314d5b700373a4a138433
avicos <adapaavinash612@gmail.com>
2024-05-28 15:31:25 +0530

    Update deletepost function in icons compoenet to handle post deletion

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,7 +5,7 @@ import {HiOutlineChat, HiOutlineHeart, HiOutlineTrash, HiHeart} from 'react-icon
 import {app} from '../firebase'
 import { getFirestore, serverTimestamp, setDoc, doc, onSnapshot, collection, deleteDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
-export default function Icons({id}) {
+export default function Icons({id, uid}) {
   const {data: session} = useSession();
   const db = getFirestore(app);
   const [isLiked, setIsLiked] = useState(false);
@@ -35,7 +35,23 @@ export default function Icons({id}) {
 
   useEffect(() => {
     setIsLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1)
-  }, [likes])
+  }, [likes]);
+
+  const deletePost = async () => {
+    if(window.confirm('Are you sure you want to delete this post?')) {
+      if(session?.user?.uid === uid) {
+      deleteDoc(doc(db, 'posts', id)).then(() => {
+      console.log('Document successfully deleted');
+      window.location.reload();
+      })
+      .catch((error) => {
+        console.log('Error removing document: ', error);
+      });
+    } else {
+      alert('You are not authorised to delete this post');
+    }
+    }
+  }
 
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
@@ -58,9 +74,11 @@ export default function Icons({id}) {
             {likes.length}
           </span>}
       </div>
-      
-       <HiOutlineTrash className='h-8 w-8 cursor-pointer rounded-full transition 
-      duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
+      {session?.user?.uid === uid && (
+        <HiOutlineTrash className='h-8 w-8 cursor-pointer rounded-full transition 
+        duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' 
+        onClick={deletePost}/>
+      )} 
     </div>
   )
 }
